1df00e4e50ad64922a6fe0f2561f35658735c5fb
Cardin-Ia <142541266+Cardin-Ia@users.noreply.github.com>
2024-04-10 17:08:22 +0000

    Wed, Apr 10, 2024, 10:08 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -1,269 +1,86 @@
-rules = document.getElementById('rules-btn')
-hiderules = document.getElementById('close-btn')
-toggle = document.querySelector('.rules')
-hide = document.querySelector('.startGame')
-start = document.getElementById('start')
+const screens = document.querySelectorAll('.screen')
+const choose_insect_btns = document.querySelectorAll('.choose-insect-btn')
+const game_container = document.getElementById('game-container')
+const start_btn = document.getElementById('start-btn')
+const timeEl = document.getElementById('time')
+const scoreEl = document.getElementById('score')
+const message = document.getElementById('message')
+const won = document.getElementById('winning')
+let seconds = 0
+let score = 0
+let selected_insect = {}
 
-const canvas = document.getElementById('canvas')
-const ctx = canvas.getContext('2d')
-
-score = 0
-first = false
-paddleFreeze = false
-
-brickRowCount = 9
-brickColumnCount = 5
-
-// Start game
-start.addEventListener('click', () => {
-    hide.classList.add('hide')
-    hide.classList.remove('btn')
-    startGame()
+start_btn.addEventListener('click',() => {
+    screens[0].classList.add('up')
 })
 
-// Rules open and close event handlers
-rules.addEventListener('click', () => {
-    toggle.classList.add('show')
+choose_insect_btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const img = btn.querySelector('img')
+        const alt = img.getAttribute('alt')
+        const src = img.getAttribute('src')
+        screens[1].classList.add('up')
+        selected_insect = {src,alt}
+        setTimeout(createInsect, 1000)
+        startGame()
+    })
 })
 
-hiderules.addEventListener('click', () => {
-    toggle.classList.remove('show')
-})
+function createInsect() {
+    const insect = document.createElement('div')
+    insect.classList.add('insect')
+    const {x,y} = getRandomLocation()
+    insect.style.top = `${y}px`
+    insect.style.left = `${x}px`
+    insect.innerHTML = `<img src="${selected_insect.src}" alt=${selected_insect.alt} style = "transform: rotate(${Math.random() * 360}deg)" />`
 
+    insect.addEventListener('click', catchInsect)
 
-// Create ball properties
-ball = {
-    x: canvas.width / 2,
-    y: canvas.height / 2,
-    size: 10,
-    speed: 4,
-    dx: 4,
-    dy: -4,
+    game_container.appendChild(insect)
 }
 
-
-// Create paddle properties
-paddle = {
-    x: canvas.width / 2 - 40,
-    y: canvas.height - 20,
-    w: 80,
-    h: 10,
-    speed: 8,
-    dx: 0,
+function catchInsect() {
+    increaseScore()
+    this.classList.add('caught')
+    setTimeout(() => this.remove(), 1000)
+    addInsects()
 }
 
-
-// Create brick properties
-brickInfo = {
-    w: 70,
-    h: 20,
-    padding: 10,
-    offsetX: 45,
-    offsetY: 60,
-    visible: true
+function addInsects() {
+    setTimeout(createInsect, 1000)
+    setTimeout(createInsect, 1500)
 }
-
-
-// Create bricks
-bricks =[]
-for (let i = 0; i < brickRowCount; i++) {
-    bricks[i] = []
-    for (let j = 0; j < brickColumnCount; j++) {
-        const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX
-        const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY
-        bricks[i][j] = {x, y, ...brickInfo}
-    }
-}
-
-// Draw ball on canvas
-function drawBall() {
-    ctx.beginPath()
-    ctx.arc(ball.x,ball.y,ball.size,0,Math.PI * 2)
-    ctx.fillStyle = '#0095dd'
-    ctx.fill()
-    ctx.closePath()
-}
-
-
-
-// Draw paddle on canvas
-function drawPaddle() {
-    ctx.beginPath()
-    ctx.rect(paddle.x,paddle.y,paddle.w,paddle.h)
-    ctx.fillStyle = '#0095dd'
-    ctx.fill()
-    ctx.closePath()
-}
-
-
-// Draw score on canvas
-function drawScore() {
-    ctx.font = '20px Arial'
-    ctx.fillText(`Score: ${score}`, canvas.width-100, 30)
-}
-
-
-// Draw bricks on canvas
-function drawBricks() {
-    bricks.forEach(column => {
-        column.forEach(brick => {
-            ctx.beginPath()
-            ctx.rect(brick.x, brick.y, brick.w, brick.h)
-            ctx.fillStyle = brick.visible ? '#0095dd' : 'transparent';            ctx.fill()
-            ctx.closePath()
-        })
-    })
-}
-
-
-
-// Draw everything
-function draw() {
-    ctx.clearRect(0,0,canvas.width,canvas.height)
-    drawPaddle()
-    drawBall()
-    drawScore()
-    drawBricks()
-}
-
-
-// Move paddle on canvas
-function movePaddle() {
-    paddle.x = paddle.x + paddle.dx
-
-    // Wall detection
-    if (paddle.x < 0) {
-        paddle.x = 0
-    }
-    if (paddle.x + paddle.w > canvas.width) {
-        paddle.x = canvas.width - paddle.w
-    }
-}
-
-
-// Keydown Event
-function keyDown(e) {
-    if (paddleFreeze == false && e.key == 'ArrowRight' || e.key == 'Right') {
-        paddle.dx = paddle.speed
-    }
-    if (paddleFreeze == false && e.key == 'ArrowLeft' || e.key == 'Left') {
-        paddle.dx = -(paddle.speed)
-    }
-}
-
-// Keyup Event
-function keyUp(e) {
-    if (paddleFreeze == false && e.key == 'ArrowRight' || e.key == 'Right' || e.key == 'ArrowLeft' || e.key == 'Left')
-    {
-        paddle.dx = 0
-    }
-}
-
-
-// Keyboard event handlers
-document.addEventListener('keydown', keyDown)
-document.addEventListener('keyup', keyUp)
-
-function moveBall() {
-    ball.x = ball.x + ball.dx
-    ball.y = ball.y + ball.dy
-
-    // Wall collision (top)
-    if (ball.y + ball.size < 0) {
-        ball.dy = -1 * ball.dy
-    }
-
-    // Wall collision (right)
-    if (ball.x + ball.size > canvas.width) {
-        ball.dx = -1 * ball.dx
-    }
-
-    // Wall collision (bottom)
-    if (ball.y + ball.size > canvas.height) {
-        ball.dy = 0
-        ball.dx = 0
-        hide.classList.add('btn')
-        hide.classList.remove('hide')
-        paddleFreeze = true
-    }
-
-    // Wall collision (left)
-    if (ball.x + ball.size < 0) {
-        ball.dx = -1 * ball.dx
-    }
-
-    // paddle collision
-    if (ball.x - ball.size > paddle.x &&
-        ball.x + ball.size < paddle.x + paddle.w &&
-        ball.y + ball.size > paddle.y) {
-            ball.dy = -1 * ball.speed
-    }
-
-    // Brick collision
-    bricks.forEach(column => {
-        column.forEach(brick => {
-            if (brick.visible) {
-                if (
-                    ball.x - ball.size > brick.x && // left brick side
-                    ball.x + ball.size < brick.x + brick.w && // right brick side
-                    ball.y + ball.size > brick.y + brick.h && // top
-                    ball.y - ball.size < brick.y + brick.h //bottom
-                    ) {
-                    ball.dy = -1 * ball.dy
-                    brick.visible = false
-                    increaseScore()
-                }
-            }
-        })
-    })
-
-}
-
-// Increase score
-function increaseScore() {
-    score++
-
-    if (score == brickRowCount * brickColumnCount) {
-        score = 0
-        showAllBricks()
-    }
-}
-
-
-function showAllBricks() {
-    bricks.forEach(column => {
-        column.forEach(brick => {
-            brick.visible = true
-        })
-    })
-
-    ball.x = canvas.width / 2
-    ball.y = canvas.height / 2
-    ball.dx = 4
-    ball.dy = -4
-}
-
-
-
-
-
-// Update canvas drawing and animation
-function update() {
-    moveBall()
-    movePaddle()
-    draw()
-    requestAnimationFrame(update)
-}
-
 
 function startGame() {
-    if (first == false) {
-        update()
-        first = true
+    setInterval(increaseTime, 1000)
+}
+
+function increaseTime() {
+    let m = Math.floor(seconds / 60)
+    let s = seconds % 60
+    if (m < 10) {
+        m = `0${m}`
     }
-    score = 0
-    showAllBricks()
-    paddleFreeze = false
-    paddle.x = canvas.width/2 - 40
-    paddle.y = canvas.height - 20
+    if (s < 10) {
+        s = `0${s}`
+    }
+    timeEl.innerHTML = `Time: ${m}:${s}`
+    seconds++
+
+}
+
+function increaseScore() {
+    score++
+    if (score > 19) {
+        message.classList.add('visible')
+    }
+    scoreEl.innerHTML = `Score: ${score}`
+}
+
+function getRandomLocation() {
+    const width = window.innerWidth
+    const height = window.innerHeight
+    const x = Math.random() * (width - 200) + 100
+    const y = Math.random() * (height - 200) + 100
+    return {x,y}
 }
