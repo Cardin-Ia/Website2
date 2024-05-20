851efed48ce915f0f80fde0345cc712b9f0d7caa
Cardin Ia <142541266+Cardin-Ia@users.noreply.github.com>
2024-05-20 14:29:52 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,20 @@
-
+btn = document.querySelector('.btn')
 button = document.getElementById('start-button')
-stuff = document.getElementById('stuff')
+link = document.getElementById('links')
 
 
 button.addEventListener('click', () => {
 button.classList.add('invisible')
-stuff.classList.add('visible')
+link.classList.add('visible')
+})
+
+
+btn.addEventListener('click', () => {
+    var randomNumber1 = Math.floor(Math.random() * 256);
+    var randomNumber2 = Math.floor(Math.random() * 256);
+    var randomNumber3 = Math.floor(Math.random() * 256);
+    var rgb = "rgb(" + randomNumber1 + "," + randomNumber2 + "," + randomNumber3 + ")";
+    document.body.style.backgroundColor = rgb;
+    document.body.classList.remove('instant-color-change');
+
 })
